fba2ae06646899134409083cf84978a090e96430
SebWend <sebekwendel12@gmail.com>
2026-05-18 14:52:53 +0200

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -1,39 +1,39 @@
 async function updateTable() {
-    try {
-        const response = await fetch('https://apis.codante.io/olympic-games/countries');
-        const data = await response.json();
-        
-        const tableBody = document.getElementById('table-body');
-        tableBody.innerHTML = ""; 
+  try {
+      const response = await fetch('https://apis.codante.io/olympic-games/countries');
+      const data = await response.json();
+      
+      const tableBody = document.getElementById('table-body');
+      tableBody.innerHTML = ""; 
 
-        data.data.forEach(country => {
-            const countryId = country.id.toUpperCase();
-            
-            const flagUrl = country.flag_url;
+      data.data.forEach(country => {
+          const countryId = country.id.toUpperCase();
+          
+          const flagUrl = country.flag_url;
 
-            const totalMedals = country.total_medals;
+          const totalMedals = country.total_medals;
 
-            const ranking = country.rank;
+          const ranking = country.rank;
 
-            const row = `
-                <tr>
-                    <td>
-                        <img src="${flagUrl}" alt="${countryId}" style="width: 20px; margin-right: 8px; vertical-align: middle;">
-                        ${countryId}
-                    </td>
-                    <td>${totalMedals}</td>
-                    <td>${ranking}</td>
-                </tr>
-            `;
-            
-            tableBody.insertAdjacentHTML('beforeend', row);
-        });
+          const row = `
+              <tr>
+                  <td>
+                      <img src="${flagUrl}" alt="${countryId}" style="width: 20px; margin-right: 8px; vertical-align: middle;">
+                      ${countryId}
+                  </td>
+                  <td>${totalMedals}</td>
+                  <td>${ranking}</td>
+              </tr>
+          `;
+          
+          tableBody.insertAdjacentHTML('beforeend', row);
+      });
 
-        console.log("Tabela zaktualizowana o flagi z flag_url!");
+      console.log("Tabela zaktualizowana o flagi z flag_url!");
 
-    } catch (error) {
-        console.error("Błąd podczas pobierania danych:", error);
-    }
+  } catch (error) {
+      console.error("Błąd podczas pobierania danych:", error);
+  }
 }
 
 updateTable();
