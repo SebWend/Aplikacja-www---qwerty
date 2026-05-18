2a46ea08e72399598cb6375dff8639f1d90f90e5
SebWend <sebekwendel12@gmail.com>
2026-05-18 13:57:06 +0200

    zmiana fetch na counties

--- fetch.js
+++ fetch.js
@@ -1,34 +1,36 @@
-(async () => {
-    // 1. Pobieramy dane z API
-    const response = await fetch('https://apis.codante.io/olympic-games/events');
-    const data = await response.json();
-    
-    // Dane z tego API są w tablicy data.data
-    const events = data.data;
-
-    // 2. Znajdujemy miejsce w HTML, gdzie chcemy wstawić wiersze
-    const tableBody = document.getElementById('table-body');
-
-    // 3. Czyścimy tabelę na wszelki wypadek
-    tableBody.innerHTML = "";
-
-    // 4. Przechodzimy przez pobrane wydarzenia (events)
-    // UWAGA: API zwraca wydarzenia, a Ty masz tabelę krajów. 
-    // Poniżej przykład, jak wygenerować wiersz na podstawie danych z API:
-    events.forEach(event => {
-        const country = event.competitors[0]?.name || "N/A";
-        const countryCode = event.competitors[0]?.country_id.toLowerCase() || "un";
-
-        // Tworzymy wiersz (HTML), który wcześniej był w index.html
-        const row = `
-            <tr>
-                <td>${country} <span class="flag">${countryCode}</span></td>
-                <td>Pobieranie...</td>
-                <td>${event.discipline_name}</td>
-            </tr>
-        `;
+async function updateTable() {
+    try {
+        // Zmieniamy adres na /countries - on zawiera informacje o medalach
+        const response = await fetch('https://apis.codante.io/olympic-games/countries');
+        const data = await response.json();
         
-        // Dodajemy wiersz do tabeli
-        tableBody.insertAdjacentHTML('beforeend', row);
-    });
-})();
+        const tableBody = document.getElementById('table-body');
+        tableBody.innerHTML = ""; // Czyścimy tabelę
+
+        // API zwraca dane w tablicy data.data
+        data.data.forEach(country => {
+            // Obliczamy całkowitą liczbę medali (suma złotych, srebrnych i brązowych)
+            const totalMedals = country.gold_medals + country.silver_medals + country.bronze_medals;
+            
+            // Przyjmijmy, że "Liczba Igrzysk" to u nas pozycja w rankingu (rank) 
+            // lub możemy tam zostawić inną informację z API.
+            const ranking = country.rank;
+
+            const row = `
+                <tr>
+                    <td>
+                        ${country.name} 
+                        <span class="flag">${country.id.toLowerCase()}</span>
+                    </td>
+                    <td>${totalMedals}</td>
+                    <td>${ranking}</td>
+                </tr>
+            `;
+            tableBody.insertAdjacentHTML('beforeend', row);
+        });
+    } catch (error) {
+        console.error("Błąd podczas aktualizacji tabeli:", error);
+    }
+}
+
+updateTable();
