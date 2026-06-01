f156f9646240de045d2d1f7d0ca4af3c3b660617
SebWend <sebekwendel12@gmail.com>
2026-06-01 21:47:11 +0200

    Delete darkmode.js

--- darkmode.js
+++
@@ -1,4 +0,0 @@
-const przycisk_darkmode=document.getElementById("darkmode")
-przycisk_darkmode.addEventListener('click', (event) => {
-    document.getElementsByTagName("body")[0].classList.toggle("darkmode")
-})
